7d421740f7585d4f55e9331f50b3850b7a7cc6c1
Taeseok Oh <Taeseok Oh@DESKTOP-063O5EN>
2025-09-10 11:46:48 +0900

    초기 커밋.

---
+++ myfirst/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World!\n");
+
+	return 0;
+}
+
+void test1()
+{
+	int age = 12;
+	const double PI = 3.141592;
+	char myname[128] = "오태석";
+	int array[] = { 1, 2, 3, 4 };
+
+	age = age + 1;
+
+	double r = 2; // 반지름 2m
+	double area = PI * r * r; // 반지름 r인 원의 넓이
+
+	if (age > 10) || (area > 5.0)) {
+
+	}
+	else {
+
+	}
+
+	for (int i = 0; i < 5; i++) {
+		printf("%d\n", i);
+	}
+}
